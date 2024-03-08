9340a713b4bbe91ff81b04c0f475ae04d197d7c6
jooda00 <dragon2000a@gmail.com>
2024-03-08 14:36:11 +0900

    test: 신입 / 신입 아닌 직원 연차 개수 차이 기능 테스트

---
+++ commute/src/test/java/com/mini/commute/entity/annual/AnnualTest.java
@@ -0,0 +1,52 @@
+package com.mini.commute.entity.annual;
+
+import com.mini.commute.entity.employee.Employee;
+import com.mini.commute.entity.employee.Role;
+import com.mini.commute.repository.employee.EmployeeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class AnnualTest {
+    private Employee employee;
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public AnnualTest(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @DisplayName("신입은 11개의 연차를 가진다.")
+    @Test
+    void getElevenAnnualIsNewbie() {
+        employee = new Employee("신입", LocalDate.of(1990,01,01), LocalDate.of(2024,01,02), Role.MEMBER);
+        employeeRepository.save(employee);
+
+        Annual annual = new Annual(employee);
+
+        employee.setAnnual(annual);
+        annual.setEmployee(employee);
+
+        assertThat(11).isEqualTo(annual.getRemainAnnual());
+    }
+
+    @DisplayName("신입이 아닌 직원은 15개의 연차를 가진다.")
+    @Test
+    void getFifteenAnnualIsNewbie() {
+        employee = new Employee("대리", LocalDate.of(1990,01,01), LocalDate.of(2023,01,02), Role.MEMBER);
+        employeeRepository.save(employee);
+
+        Annual annual = new Annual(employee);
+
+        employee.setAnnual(annual);
+        annual.setEmployee(employee);
+
+        assertThat(15).isEqualTo(annual.getRemainAnnual());
+    }
+}
